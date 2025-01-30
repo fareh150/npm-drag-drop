ebce78ee787f30b176019e640f611e4193d28066
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2025-01-30 14:09:45 +0100

    fatal error

--- projects/drag-drop-sort-dialog-testbed-app/src/app/app.component.ts
+++ projects/drag-drop-sort-dialog-testbed-app/src/app/app.component.ts
@@ -1,15 +1,75 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
 import { DragDropSortDialogComponent } from 'drag-drop-sort-dialog';
 
 @Component({
   selector: 'app-root',
-  imports: [
-    DragDropSortDialogComponent
-  ],
+  imports: [DragDropSortDialogComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'drag-drop-sort-dialog-testbed-app';
+
+  items =  [
+    {
+      sort: 1,
+      name: 'item 1'
+    },
+    {
+      sort: 2,
+      name: 'item 2'
+    },
+    {
+      sort: 3,
+      name: 'item 3'
+    },
+    {
+      sort: 4,
+      name: 'item 4'
+    },
+    {
+      sort: 5,
+      name: 'item 5'
+    },
+    {
+      sort: 6,
+      name: 'item 6'
+    },
+    {
+      sort: 7,
+      name: 'item 7'
+    },
+    {
+      sort: 8,
+      name: 'item 8'
+    },
+    {
+      sort: 9,
+      name: 'item 9'
+    },
+    {
+      sort: 10,
+      name: 'item 10'
+    }
+  ]
+
+  constructor(public dialog: MatDialog) {}
+
+
+  openDialog() {
+    this.dialog.open(DragDropSortDialogComponent, {
+         width: '30vw',
+         maxWidth: '30vw',
+         minWidth: '240px',
+         maxHeight: '90vh',
+         autoFocus: false,
+         data: {
+           sortableData: this.items,
+           displayKey: 'name',
+           orderKey: 'sort'
+         },
+       })
+  }
 }
--- projects/drag-drop-sort-dialog/src/lib/drag-drop-sort-dialog.component.ts
+++ projects/drag-drop-sort-dialog/src/lib/drag-drop-sort-dialog.component.ts
@@ -1,15 +1,75 @@
-import { Component } from '@angular/core';
+import { Component, Inject, signal, WritableSignal } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MatIcon } from '@angular/material/icon';
+import { CdkDragDrop, moveItemInArray, CdkDrag, CdkDropList } from '@angular/cdk/drag-drop';
+
+type SortableItem = Record<string, any>;
 
 @Component({
   selector: 'lib-drag-drop-sort-dialog',
-  imports: [],
-  template: `
-    <p>
-      drag-drop-sort-dialog works!
-    </p>
-  `,
+  imports: [
+    MatIcon,
+    CdkDrag,
+    CdkDropList,
+  ],
+  templateUrl: './drag-drop-sort-dialog.component.html',
   styles: ``
 })
 export class DragDropSortDialogComponent {
+    sortableData: WritableSignal<SortableItem[]> = signal([]);
+    initialData: WritableSignal<SortableItem[]> = signal([]);
+    modifiedData: WritableSignal<SortableItem[]> = signal([]);
+    displayKey: WritableSignal<string> = signal('');
+    orderKey: WritableSignal<string> = signal('');
 
+    constructor(
+        @Inject(MAT_DIALOG_DATA) public data: {
+            sortableData: SortableItem[],
+            displayKey: string,
+            orderKey: string,
+        },
+        private readonly dialogRef: MatDialogRef<DragDropSortDialogComponent>,
+    ) {
+        this.modifiedData.set(this.sortByOrderKey(data.sortableData, data.orderKey));
+        this.sortableData.set(this.modifiedData());
+        this.initialData.set(this.modifiedData());
+        this.displayKey.set(data.displayKey);
+        this.orderKey.set(data.orderKey);
+    }
+
+    private sortByOrderKey(data: SortableItem[], orderKey: string): SortableItem[] {
+        return [...data].sort((a, b) => {
+            if (a[orderKey] < b[orderKey]) {
+                return -1;
+            }
+            if (a[orderKey] > b[orderKey]) {
+                return 1;
+            }
+            return 0;
+        });
+    }
+
+    drop(event: CdkDragDrop<SortableItem[]>): void {
+        moveItemInArray(this.sortableData(), event.previousIndex, event.currentIndex);
+
+        this.sortableData.set(this.sortableData().map((item, index) => {
+            return {
+                ...item,
+                [this.orderKey()]: index + 1,
+            };
+        }));
+    }
+
+    async saveData(): Promise<void> {
+        const changedItems = this.sortableData().filter((modifiedItem, index) => {
+            const initialItem = this.initialData().find(item => item[this.displayKey()] === modifiedItem[this.displayKey()]);
+            return initialItem && initialItem[this.orderKey()] !== modifiedItem[this.orderKey()];
+        });
+
+        this.closeSortDialog(changedItems.length > 0 ? changedItems : undefined);
+    }
+
+    closeSortDialog(changedData?: SortableItem[]): void {
+        this.dialogRef.close(changedData);
+    }
 }
